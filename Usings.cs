b425b572f9f43c6a721690f92145b5dfa20b43be
Alexandra Vescan <alexandra.vescan@altom.com>
2023-05-24 14:17:51 +0300

    updated C# to version 10 so it accepts global Usings and added the Usings file

---
+++ Usings.cs
@@ -0,0 +1,14 @@
+global using Altom.AltDriver; 
+global using Newtonsoft.Json;
+global using System;
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.Threading;
+
+global using alttrashcat_tests_csharp.pages;
+global using NUnit.Framework;
+global using Allure;
+global using NUnit.Allure.Core;
+global using NUnit.Allure.Attributes;
+global using Allure.Commons;
+global using System.Drawing;
